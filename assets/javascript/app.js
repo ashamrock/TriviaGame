0d55c9a5052ab0150c48b0bc60e89727306fdb02
Andrew Shamrock <a.shamrock4@gmail.com>
2019-09-07 15:22:08 -0400

    countdown and alerts working. missplelled incremental

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,25 +4,34 @@ $(document).ready(function(){
     
   //  var wrong = 0;
     
-    var time = 30;
+    var time = 10;
 
     var btnclk = 0;
-
-    setTimeout(timeout, 1000 * 5);
     
+  
+setInterval(function(){
+        time--;
+        $("#time-left").html(time);
+        if (time === 0) {
+            alert("all done! you answered " + right + " out of 5! Good job?");
+            time = "done!";
+
+        }
+      }, 1000);
+
+
     $("button").click(function(){
         $(this).closest(".question").hide(1000);
         btnclk++;
         console.log (btnclk)
         if (btnclk === 2) {
+            time = "done!";
             alert("all done!");
             alert("Lets see how you did");
             alert("You answered " + right + " out of 5! Good job?");
           }
     });
 
-    function timeout() {
-        alert("all done! you answered " + right + " out of 5! Good job?");
-        }
-          
+
 });
+
