ab739e459a5c35ad7ad841aaf16d33d92c3531ef
Andrew Shamrock <a.shamrock4@gmail.com>
2019-09-08 08:29:51 -0400

    timer working. buttons adding. change alerts to html messages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,36 +2,32 @@ $(document).ready(function(){
 
     var right = 0;
     
-  //  var wrong = 0;
-    
-    var time = 10;
+    var time = 31;
 
     var btnclk = 0;
     
   
 setInterval(function(){
-        time--;
-        $("#time-left").html(time);
-        if (time === 0) {
-            alert("all done! you answered " + right + " out of 5! Good job?");
-            time = "done!";
+  time--;
+  $("#time-left").html(time);
+    if (time === 0) {
+    alert("all done! you answered " + right + " out of 5! Good job?");
+    time = "done!";
+    }
+}, 1000);
 
-        }
-      }, 1000);
-
-
-    $("button").click(function(){
-        $(this).closest(".question").hide(1000);
-        btnclk++;
-        console.log (btnclk)
-        if (btnclk === 2) {
-            time = "done!";
-            alert("all done!");
-            alert("Lets see how you did");
-            alert("You answered " + right + " out of 5! Good job?");
-          }
+$("button").click(function(){
+  $(this).closest(".question").hide(1000);
+    btnclk++;
+    right = Number(right) + Number($(this).val());
+    console.log (right)
+    if (btnclk === 2) {
+      time = "done!";
+      alert("all done!");
+      alert("Lets see how you did");
+      alert("You answered " + right + " out of 5! Good job?");
+      }
     });
-
 
 });
 
